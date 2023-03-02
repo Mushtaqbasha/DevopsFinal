3ec1fb05b8bb66d5f80011f98ee07684722832ca
Mush <mushtaqbasha7.mb@gmail.com>
2023-03-02 09:54:45 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,9 +6,9 @@ function Home() {
     <div>
       <div className="banner-image"></div>
       <div>
-        <span className="tagline">JUST DOES IT</span>
+        <span className="tagline">JUST DO IT</span>
         <div className="tagline-para">
-          Nike has been your compadsdvsdvnion in happiness, both big and small, for
+          Nike has been your companion in happiness, both big and small, for
           over 50 years. Our roots are firmly grounded in India and with its
           people.
         </div>
@@ -18,7 +18,7 @@ function Home() {
         <br />
         <div className="latest-container">
           <hr className="line" /> &nbsp; &nbsp;
-          <span className="latest-designs">Latest Desigerwsns</span>&nbsp;&nbsp;
+          <span className="latest-designs">Latest Designs</span>&nbsp;&nbsp;
           <hr className="line" />
           <br />
           <br />
@@ -32,8 +32,8 @@ function Home() {
                     alt={shoe.name}
                     src={shoe.img}
                   />
-                  <h3 className="shoe-name">{shoe.name} jajhaja</h3>
-                  <h3 className="shoe-price">${shoe.price} sdhvcuwvyciwy</h3>
+                  <h3 className="shoe-name">{shoe.name} </h3>
+                  <h3 className="shoe-price">${shoe.price} </h3>
                 </div>
               );
             })}
